cf13ecd4dc15624dabcb726c8c3661c357efd1fa
Pavel Zhelukevich <pashazhelukevich1@gmail.com>
2023-02-18 22:45:01 +0300

    components

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,16 @@
 <template >
-  <form @submit.prevent>
-    <h2>Создание поста</h2>
-    <input @input="title = $event.target.value" v-bind:value="title" type="text" placeholder="Название">
-    <input @input="body = $event.target.value" v-bind:value="body" type="text" placeholder="Описание">
-    <button @click="createPost">Добавить</button>
-  </form>
-
-  <div class="post" v-for="post in posts">
-    <div><strong>Название:</strong> {{ post.title }}</div>
-    <div><strong>Описание:</strong> {{ post.body }}</div>
-  </div>
+  <PostForm @create="createPost" />
+  <PostList :posts="posts" />
 </template>
 
 <script>
+import PostForm from '@/components/PostForm.vue';
+import PostList from '@/components/PostList.vue';
+
 export default {
+  components: {
+    PostForm, PostList
+  },
   data() {
     return {
       posts: [
@@ -21,21 +18,11 @@ export default {
         { id: 2, title: 'JavaScript 2', body: 'Описание поста 2' },
         { id: 3, title: 'JavaScript 3', body: 'Описание поста 3' },
       ],
-      title: '',
-      body: '',
     }
   },
   methods: {
-    createPost() {
-      const newPost = {
-        id: Date.now(),
-        title: this.title,
-        body: this.body,
-      }
-      this.posts.push(newPost);
-      this.title = '';
-      this.body = '';
-
+    createPost(post) {
+      this.posts.push(post)
     },
   },
 }
@@ -46,11 +33,5 @@ export default {
   margin: 0;
   padding: 0;
   box-sizing: border-box;
-}
-
-.post {
-  padding: 15px;
-  border: 2px solid teal;
-  margin-top: 15px;
 }
 </style>
---
+++ src/components/PostForm.vue
@@ -0,0 +1,33 @@
+<template >
+  <form @submit.prevent>
+    <h2>Создание поста</h2>
+    <input v-model="post.title" type="text" placeholder="Название">
+    <input v-model="post.body" type="text" placeholder="Описание">
+    <button @click="createPost">Добавить</button>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      post: {
+        title: '',
+        body: '',
+      }
+    }
+  },
+  methods: {
+    createPost() {
+      this.post.id = Date.now();
+      this.$emit('create', this.post)
+      this.post = {
+        title: '',
+        body: '',
+      }
+    },
+  }
+}
+</script>
+
+<style ></style>
