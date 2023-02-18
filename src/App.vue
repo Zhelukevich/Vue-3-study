98da5881322ccf6b62e98d400a58ff8188b0b975
Pavel Zhelukevich <pashazhelukevich1@gmail.com>
2023-02-18 21:56:57 +0300

    add createPost

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
 <template >
-  <button v-on:click="addLike">+</button>
-  <button v-on:click="addDislike">-</button>
-  <div>
-    лайк <strong>{{ likes }}</strong><br>
-    дизлайк <strong>{{ dislikes }}</strong>
+  <form @submit.prevent>
+    <h2>Создание поста</h2>
+    <input @input="title = $event.target.value" v-bind:value="title" type="text" placeholder="Название">
+    <input @input="body = $event.target.value" v-bind:value="body" type="text" placeholder="Описание">
+    <button @click="createPost">Добавить</button>
+  </form>
+
+  <div class="post" v-for="post in posts">
+    <div><strong>Название:</strong> {{ post.title }}</div>
+    <div><strong>Описание:</strong> {{ post.body }}</div>
   </div>
 </template>
 
@@ -11,19 +16,41 @@
 export default {
   data() {
     return {
-      likes: 0,
-      dislikes: 0,
+      posts: [
+        { id: 1, title: 'JavaScript', body: 'Описание поста' },
+        { id: 2, title: 'JavaScript 2', body: 'Описание поста 2' },
+        { id: 3, title: 'JavaScript 3', body: 'Описание поста 3' },
+      ],
+      title: '',
+      body: '',
     }
   },
   methods: {
-    addLike() {
-      this.likes += 1;
-    },
-    addDislike() {
-      this.dislikes -= 1;
+    createPost() {
+      const newPost = {
+        id: Date.now(),
+        title: this.title,
+        body: this.body,
+      }
+      this.posts.push(newPost);
+      this.title = '';
+      this.body = '';
+
     },
   },
 }
 </script>
 
-<style ></style>
+<style >
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+.post {
+  padding: 15px;
+  border: 2px solid teal;
+  margin-top: 15px;
+}
+</style>
