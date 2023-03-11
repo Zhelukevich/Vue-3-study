ede3ca2d12852f1daf4353a0a47aa0056d08c6a3
Pavel Zhelukevich <pashazhelukevich1@gmail.com>
2023-03-11 16:04:27 +0300

    add axios

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,7 @@
 <script>
 import PostForm from '@/components/PostForm.vue';
 import PostList from '@/components/PostList.vue';
+import axios from 'axios';
 
 export default {
   components: {
@@ -19,11 +20,7 @@ export default {
   },
   data() {
     return {
-      posts: [
-        { id: 1, title: 'JavaScript', body: 'Описание поста' },
-        { id: 2, title: 'JavaScript 2', body: 'Описание поста 2' },
-        { id: 3, title: 'JavaScript 3', body: 'Описание поста 3' },
-      ],
+      posts: [],
       dialogVisible: false,
     }
   },
@@ -37,6 +34,14 @@ export default {
     },
     showDialog() {
       this.dialogVisible = true;
+    },
+    async fetchPosts() {
+      try {
+        const response = await axios.get('https://jsonplaceholder.typicode.com/posts?_limit=10');
+        this.posts = response.data;
+      } catch (error) {
+
+      }
     }
   }
 }
